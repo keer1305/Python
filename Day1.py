1e4a7cee51ccdbe751709806f638cac512d6b2ed
Keerthana Sathiyamoorthy <42031697+keer1305@users.noreply.github.com>
2024-05-29 23:19:53 -0400

    day 1

---
+++ Day1.py
@@ -0,0 +1,14 @@
+print("hello")
+"""
+a = 10
+name = input("whats ur name : ")
+print("hello" +name)
+
+birth_year = input("enter your birth year: ") #input always gets it like string
+age = 2024 - int(birth_year)
+print(age)
+"""
+
+print('keer\'s name') #using \ to skip it
+print(r'c:\keer\nlvideo') #using raw string to print it as to skip special char like \n
+
