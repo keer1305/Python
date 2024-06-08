2b347ec66bb1778a0cff887e1dfd16164df37c46
keer1305 <13keerthana@gmail.com>
2024-06-08 10:04:31 -0400

    import math ,user input functions & argv

--- Day4.py
+++ Day4.py
@@ -1,1 +1,28 @@
 #Varaible address
+num=5
+print(id(num))
+
+name = 'keer'
+print(id(name))
+
+a=10
+b=a
+print(a)
+
+print(id(a))
+print(id(b))
+print(id(10))
+
+#Datatypes in python
+
+#SWAP 2 VARIABLES IN PYTHON
+
+a=5
+b=6
+
+temp = a #save the a value in temp variable to assign it to b later
+a = b  #saving b value to a
+b = temp  #saving temp 5 value to b
+
+print(a)
+print(b)
---
+++ Day5.py
@@ -0,0 +1,32 @@
+import math
+import math as m
+from math import sqrt,pow
+
+
+print(math.pow(3,2))
+print(math.pi)
+print(math.e)
+
+#user input in python
+# x=input("enter the x:   " ) #by default gives the type as string ,so convert to int
+# print(type(x))
+# a=int(x)
+# y=int(input("enter the y:  " ))
+# z=a+y
+# print(z)
+
+#character
+# ch = input("enter a char: ")
+# print(ch)
+# print(ch[0])
+
+result=eval(input('enter an expr'))
+print(result)
+
+#argv argument variable
+# sys.argv[0]: The name of the script.
+# sys.argv[1]: The first argument.
+# sys.argv[2]: The second argument, and so on.
+#
+# python example.py arg1 arg2 arg3
+
