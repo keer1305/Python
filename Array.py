622fb63b8e4e54bea6512fcaabdead07a554d314
keer1305 <13keerthana@gmail.com>
2024-06-20 20:53:59 -0400

    Array using for loop and printing the array values

--- Array.py
+++ Array.py
@@ -2,3 +2,40 @@
 #we can add elements and find element using methods and index number of parti. value
 #all values of same type
 
+import array as arr
+from array import *
+
+
+val = array('i',[5,-7,6,4])
+
+for i in range(4):
+    print(val[i])
+
+val.reverse()
+print(val)
+
+for i in range(len(val)):  #dynamic
+    print(val[i])
+
+for e in val:
+    print(e)
+
+
+#using characters:
+
+val = array('u',['a','r','i'])
+print(val)
+
+val = array('i',[2,5,8,5,11,342,12])
+newArray = array(val.typecode,(a*a for a in val))
+print(newArray)
+
+for i in newArray:
+    print(i)
+
+#using while:
+
+i=0
+while i<len(newArray):
+    print(newArray[i])
+    i+=1
